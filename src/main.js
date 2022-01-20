cc5c557fa602940020c81672763c607322f24e70
Christian Neij <82954891+Loathed94@users.noreply.github.com>
2022-01-20 16:26:40 +0100

    skitsamma

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store'
+import router from './router'
 
-createApp.store;
+createApp.store = store; //??
+createApp.router = router; //??
 createApp(App).mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,2 +1,32 @@
+import { processExpression } from '@vue/compiler-core';
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+
+Vue.use(VueRouter);
+
+const routes = [
+    {
+        path: '/',
+        name: "Login",
+        component: () => import(/* webpackChunkName: "login"*/ "../components/Login/Login.vue")
+    },
+    {
+        path: '/movies',
+        name: 'Movies',
+        component: () => import(/* webpackChunkName: "movies" */ "../components/Movie/Movies.vue")
+    },
+    {
+        path: '/profile',
+        name: 'Profile',
+        component: () => import(/* webpackChunkName: "profile"*/ "../components/Login/Profile.vue")
+    }
+
+]
+
+const router = new VueRouter({
+    mode: "history",
+    base: process.env.BASE_URL,
+    routes
+})
+
+export default router;

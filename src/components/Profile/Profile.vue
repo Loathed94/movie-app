74746c20790c51721b4ab3b9d247cece7354c9c8
Christian Neij <82954891+Loathed94@users.noreply.github.com>
2022-01-20 08:27:03 +0100

    feat: added vue-files.

---
+++ src/components/Profile/Profile.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <button>Go to Movies</button>
+        <h3>Favourite Movies List</h3>
+        <ul>
+
+        </ul>
+
+    </div>
+</template>
+
+<script>
+export default{
+    name: "Movies"
+}
+</script>
+
+<style scoped>
+
+</style>
